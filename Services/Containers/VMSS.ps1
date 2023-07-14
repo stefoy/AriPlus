07abbc8e8c3ae7dd9110f7daaa2fb2fef45f5c91
Stephen <foy@chauce.me>
2023-07-14 11:45:14 +0800

    Update VMSS.ps1

--- Services/Containers/VMSS.ps1
+++ Services/Containers/VMSS.ps1
@@ -56,13 +56,10 @@ If ($Task -eq 'Processing')
                     'ID'                            = $1.id;
                     'Subscription'                  = $sub1.Name;
                     'Resource Group'                = $1.RESOURCEGROUP;
-                    'AKS / SFC'                     = $Related;
+                    'AKS'                     = $Related;
                     'Name'                          = $1.NAME;
                     'Location'                      = $1.LOCATION;
                     'SKU Tier'                      = $1.sku.tier;
-                    'Fault Domain'                  = $data.platformFaultDomainCount;
-                    'Upgrade Policy'                = $data.upgradePolicy.mode;                                    
-                    'Diagnostics'                   = $Diag;
                     'VM Size'                       = $1.sku.name;
                     'Instances'                     = $1.sku.capacity;
                     'Autoscale Enabled'             = $AutoSc;
@@ -73,14 +70,11 @@ If ($Task -eq 'Processing')
                     'Image Version'                 = $data.virtualMachineProfile.storageProfile.imageReference.sku;                            
                     'VM OS Disk Size (GB)'          = $data.virtualMachineProfile.storageProfile.osDisk.diskSizeGB;
                     'Disk Storage Account Type'     = $data.virtualMachineProfile.storageProfile.osDisk.managedDisk.storageAccountType;
-                    'Disable Password Authentication'= $disablePwd;
-                    'Custom DNS Servers'            = [string]$data.virtualMachineProfile.networkProfile.networkInterfaceConfigurations.properties.dnsSettings.dnsServers;
                     'Virtual Network'               = $VNET;
                     'Subnet'                        = $Subnet;
                     'Accelerated Networking Enabled'= $data.virtualMachineProfile.networkProfile.networkInterfaceConfigurations.properties.enableAcceleratedNetworking; 
                     'Network Security Group'        = $NSG;
                     'Extensions'                    = [string]$ext;
-                    'Admin Username'                = $data.virtualMachineProfile.osProfile.adminUsername;
                     'VM Name Prefix'                = $data.virtualMachineProfile.osProfile.computerNamePrefix;
                     'Created Time'                  = $timecreated;
                     'Resource U'                    = $ResUCount;
@@ -103,28 +97,16 @@ Else
 
         $TableName = ('VMSSTable_'+($SmaResources.VMSS.id | Select-Object -Unique).count)
         $Style = @()        
-        $Style += New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat '0' -Range A:W
-        $Style += New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat '0.0' -Range Y:AA
-        $Style += New-ExcelStyle -HorizontalAlignment Left -Range W:W -Width 60 -WrapText
 
         $condtxt = @()
-        $condtxt += New-ConditionalText FALSE -Range L:L
-        $condtxt += New-ConditionalText FALSO -Range L:L
-        $condtxt += New-ConditionalText Disabled -Range I:I
-        $condtxt += New-ConditionalText FALSE -Range V:V
-        $condtxt += New-ConditionalText FALSO -Range V:V
-
 
         $Exc = New-Object System.Collections.Generic.List[System.Object]
         $Exc.Add('Subscription')
         $Exc.Add('Resource Group')
-        $Exc.Add('AKS / SFC')
+        $Exc.Add('AKS')
         $Exc.Add('Name')
         $Exc.Add('Location')
         $Exc.Add('SKU Tier')
-        $Exc.Add('Fault Domain')
-        $Exc.Add('Upgrade Policy')                                   
-        $Exc.Add('Diagnostics')
         $Exc.Add('VM Size')
         $Exc.Add('vCPUs')
         $Exc.Add('RAM')
@@ -135,14 +117,10 @@ Else
         $Exc.Add('Image Version')                        
         $Exc.Add('VM OS Disk Size (GB)')
         $Exc.Add('Disk Storage Account Type')
-        $Exc.Add('Disable Password Authentication')
-        $Exc.Add('Custom DNS Servers')
         $Exc.Add('Virtual Network')
         $Exc.Add('Subnet')
         $Exc.Add('Accelerated Networking Enabled')
         $Exc.Add('Network Security Group')
-        $Exc.Add('Extensions')
-        $Exc.Add('Admin Username')
         $Exc.Add('VM Name Prefix')
         $Exc.Add('Created Time')
 
