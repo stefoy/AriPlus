1dbafa18ffd03322cb7284f356bdac083a154245
Stephen <foy@chauce.me>
2023-07-14 10:58:37 +0800

    Update VNETGTW.ps1

--- Services/Networking/VNETGTW.ps1
+++ Services/Networking/VNETGTW.ps1
@@ -20,16 +20,6 @@ If ($Task -eq 'Processing') {
                     'Location'               = $1.LOCATION;
                     'SKU'                    = $data.sku.tier;
                     'Active-active mode'     = $data.activeActive; 
-                    'Gateway Type'           = $data.gatewayType;
-                    'Gateway Generation'     = $data.vpnGatewayGeneration;
-                    'VPN Type'               = $data.vpnType;
-                    'Enable Private Address' = $data.enablePrivateIpAddress;
-                    'Enable BGP'             = $data.enableBgp;
-                    'BGP ASN'                = $data.bgpsettings.asn;
-                    'BGP Peering Address'    = $data.bgpSettings.bgpPeeringAddress;
-                    'BGP Peer Weight'        = $data.bgpSettings.peerWeight;
-                    'Gateway Public IP'      = [string]$data.ipConfigurations.properties.publicIPAddress.id.split("/")[8];
-                    'Gateway Subnet Name'    = [string]$data.ipConfigurations.properties.subnet.id.split("/")[8];
                     'Resource U'             = $ResUCount;
                 }
                 $tmp += $obj
@@ -53,16 +43,6 @@ Else {
         $Exc.Add('Location')
         $Exc.Add('SKU')
         $Exc.Add('Active-active mode')
-        $Exc.Add('Gateway Type')
-        $Exc.Add('Gateway Generation')
-        $Exc.Add('VPN Type')
-        $Exc.Add('Enable Private Address')
-        $Exc.Add('Enable BGP')
-        $Exc.Add('BGP ASN')
-        $Exc.Add('BGP Peering Address')
-        $Exc.Add('BGP Peer Weight')
-        $Exc.Add('Gateway Public IP')
-        $Exc.Add('Gateway Subnet Name')
 
 
         $ExcelVar = $SmaResources.VNETGTW 
