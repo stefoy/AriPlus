98871d05a615e5a4dbc10298c83eea754514edf0
Stephen <foy@chauce.me>
2023-07-25 12:33:41 -0700

    Update SQLMI.ps1

--- Services/Data/SQLMI.ps1
+++ Services/Data/SQLMI.ps1
@@ -1,45 +1,55 @@
 param($SCPath, $Sub, $Resources, $Task ,$File, $SmaResources, $TableStyle, $Metrics)
 
-if ($Task -eq 'Processing') {
-
+if ($Task -eq 'Processing') 
+{
     $SQLSERVERMI = $Resources | Where-Object { $_.TYPE -eq 'microsoft.sql/managedInstances' }
+    $SQLSERVERMIDB = $Resources | Where-Object { $_.TYPE -eq 'microsoft.sql/managedinstances/databases' }
+
+    "id": "/subscriptions/20d7082a-0fc7-4468-82bd-542694d5042b/resourceGroups/Test1/providers/Microsoft.Sql/managedInstances/testinstance1",
+    "id": "/subscriptions/00000000-1111-2222-3333-444444444444/resourceGroups/Test1/providers/Microsoft.Sql/managedInstances/testcl/databases/testdb1",
+
 
     if($SQLSERVERMI)
+    {
+        $tmp = @()
+
+        foreach ($1 in $SQLSERVERMI) 
         {
-            $tmp = @()
+            $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
+            $data = $1.PROPERTIES
+            $databases = $SQLSERVERMIDB | Where-Object { $_.Id -contains $1.Id }
 
-            foreach ($1 in $SQLSERVERMI) {
-                $ResUCount = 1
-                $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
-                $data = $1.PROPERTIES
-
-                    $obj = @{
-                        'ID'                            = $1.id;
-                        'Subscription'                  = $sub1.Name;
-                        'ResourceGroup'                 = $1.RESOURCEGROUP;
-                        'Name'                          = $1.NAME;
-                        'Location'                      = $1.LOCATION;
-                        'SkuName'                       = $1.sku.Name;
-                        'SkuCapacity'                   = $1.sku.capacity;
-                        'SkuTier'                       = $1.sku.tier;
-                        'SkuFamily'                     = $1.sku.family;
-                        'InstancePoolName'              = $data.instancePoolId;
-                        'vCores'                        = $data.vCores;
-                        'StorageGB'                     = $data.storageSizeInGB;
-                        'StorageAccountType'            = $data.storageAccountType;
-                        'LicenseType'                   = $data.licenseType;
-                        'State'                         = $data.state;
-                        'ManagedInstanceCreateMode'     = $data.managedInstanceCreateMode;
-                        'ZoneRedundant'                 = $data.zoneRedundant;
-                    }
-                    $tmp += $obj        
+            $obj = @{
+                'ID'                            = $1.id;
+                'Subscription'                  = $sub1.Name;
+                'ResourceGroup'                 = $1.RESOURCEGROUP;
+                'Name'                          = $1.NAME;
+                'Location'                      = $1.LOCATION;
+                'SkuName'                       = $1.sku.Name;
+                'SkuCapacity'                   = $1.sku.capacity;
+                'SkuTier'                       = $1.sku.tier;
+                'SkuFamily'                     = $1.sku.family;
+                'InstancePoolName'              = $data.instancePoolId;
+                'vCores'                        = $data.vCores;
+                'StorageGB'                     = $data.storageSizeInGB;
+                'StorageAccountType'            = $data.storageAccountType;
+                'LicenseType'                   = $data.licenseType;
+                'State'                         = $data.state;
+                'ManagedInstanceCreateMode'     = $data.managedInstanceCreateMode;
+                'ZoneRedundant'                 = $data.zoneRedundant;
+                'Databases'                     = if ($null -ne $databases) { $databases.Count } else { '0' }
             }
             
-            $tmp
+            $tmp += $obj        
         }
+        
+        $tmp
+    }
 }
-else {
-    if ($SmaResources.SQLMI) {
+else 
+{
+    if ($SmaResources.SQLMI) 
+    {
 
         $TableName = ('SQLMITable_'+($SmaResources.SQLMI.id | Select-Object -Unique).count)
         $Style = New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat 0
@@ -64,12 +74,12 @@ else {
         $Exc.Add('vCores')
         $Exc.Add('ManagedInstanceCreateMode')
         $Exc.Add('ZoneRedundant')
+        $Exc.Add('Databases')
 
         $ExcelVar = $SmaResources.SQLMI
 
         $ExcelVar | 
         ForEach-Object { [PSCustomObject]$_ } | Select-Object -Unique $Exc | 
         Export-Excel -Path $File -WorksheetName 'SQL MI' -AutoSize -MaxAutoSizeRows 100 -TableName $TableName -TableStyle $tableStyle -ConditionalText $condtxt -Style $Style
-
     }
 }
