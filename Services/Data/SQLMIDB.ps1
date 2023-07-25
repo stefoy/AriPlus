7e72f04b074be010c3c5fcb7e8193e30ad253a23
Stephen <foy@chauce.me>
2023-07-25 12:10:51 -0700

    Update SQLMIDB.ps1

--- Services/Data/SQLMIDB.ps1
+++ Services/Data/SQLMIDB.ps1
@@ -1,53 +1,46 @@
 param($SCPath, $Sub, $Resources, $Task ,$File, $SmaResources, $TableStyle, $Metrics)
 
-if ($Task -eq 'Processing') {
-
+if ($Task -eq 'Processing') 
+{
     $SQLSERVERMIDB = $Resources | Where-Object { $_.TYPE -eq 'microsoft.sql/managedinstances/databases' }
 
     if($SQLSERVERMIDB)
-        {
-            $tmp = @()
+    {
+        $tmp = @()
 
-            foreach ($1 in $SQLSERVERMIDB) {
-                $ResUCount = 1
-                $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
-                $data = $1.PROPERTIES
+        foreach ($1 in $SQLSERVERMIDB) {
+            $sub1 = $SUB | Where-Object { $_.id -eq $1.subscriptionId }
+            $data = $1.PROPERTIES
 
-                $pvteps = if(!($data.privateEndpointConnections)) {[pscustomobject]@{id = 'NONE'}} else {$data.privateEndpointConnections | Select-Object @{Name="id";Expression={$_.id.split("/")[10]}}}
-
-                foreach ($pvtep in $pvteps) {
-                    $obj = @{
-                        'ID'                    = $1.id;
-                        'Subscription'          = $sub1.Name;
-                        'MI parent'        = $1.id.split("/")[8];
-                        'Name'                  = $1.NAME;
-                        'Collation'              = $data.collation;
-                        'CreationDate'               = $data.creationDate;
-                        'DefaultSecondaryLocation'               = $data.defaultSecondaryLocation;
-                        'Status'           = $data.status;
-                    }
-                    $tmp += $obj
-                    if ($ResUCount -eq 1) { $ResUCount = 0 } 
-                }          
-            }
-            $tmp
+            $obj = @{
+                'ID'                        = $1.id;
+                'Subscription'              = $sub1.Name;
+                'ManagedInstance'           = $1.id.split("/")[8];
+                'Name'                      = $1.NAME;
+                'Collation'                 = $data.collation;
+                'CreationDate'              = $data.creationDate;
+                'DefaultSecondaryLocation'  = $data.defaultSecondaryLocation;
+                'Status'                    = $data.status;   
+            } 
+            
+            $tmp += $obj
         }
+        
+        $tmp
+    }
 }
-else {
-    if ($SmaResources.SQLMIDB) {
-
+else 
+{
+    if ($SmaResources.SQLMIDB) 
+    {
         $TableName = ('SQLMIDBTable_'+($SmaResources.SQLMIDB.id | Select-Object -Unique).count)
         $Style = New-ExcelStyle -HorizontalAlignment Center -AutoSize -NumberFormat 0
 
         $condtxt = @()
-        $condtxt += New-ConditionalText FALSE -Range J:J
-        $condtxt += New-ConditionalText FALSO -Range J:J
-        $condtxt += New-ConditionalText FAUX -Range J:J
-        $condtxt += New-ConditionalText offline -Range G:G
 
         $Exc = New-Object System.Collections.Generic.List[System.Object]
         $Exc.Add('Subscription')
-        $Exc.Add('MI parent')
+        $Exc.Add('ManagedInstance')
         $Exc.Add('Name')
         $Exc.Add('Collation')
         $Exc.Add('CreationDate')
@@ -59,6 +52,5 @@ else {
         $ExcelVar | 
         ForEach-Object { [PSCustomObject]$_ } | Select-Object -Unique $Exc | 
         Export-Excel -Path $File -WorksheetName 'SQL MI DBs' -AutoSize -MaxAutoSizeRows 100 -TableName $TableName -TableStyle $tableStyle -ConditionalText $condtxt -Style $Style
-
     }
 }
