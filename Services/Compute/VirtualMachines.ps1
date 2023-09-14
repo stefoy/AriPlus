750f858003e813a9ff9fae70057ca6bc25abe94a
Stephen <foy@chauce.me>
2023-09-14 12:29:34 +0800

    Update VirtualMachines.ps1

--- Services/Compute/VirtualMachines.ps1
+++ Services/Compute/VirtualMachines.ps1
@@ -4,7 +4,8 @@ If ($Task -eq 'Processing')
 {
     $virtualMachines =  $Resources | Where-Object { $_.TYPE -eq 'microsoft.compute/virtualmachines' } 
     $virtualMachineMetrics = $Metrics.Metrics | Where-Object { $_.Service -eq 'Virtual Machines' }
-
+    $disk = $Resources | Where-Object {$_.TYPE -eq 'microsoft.compute/disks'}
+    
     $vmsizemap = @{}
 
     foreach($location in ($virtualMachines | Select-Object -ExpandProperty location -Unique))
@@ -38,6 +39,17 @@ If ($Task -eq 'Processing')
 
             $Lic = if($Lic) { $Lic } else { 'License Included' }
 
+            if($data.storageProfile.osDisk.managedDisk.id) 
+            {
+               $OSDisk = ($disk | Where-Object {$_.id -eq $data.storageProfile.osDisk.managedDisk.id} | Select-Object -Unique).sku.name
+               $OSDiskSize = ($disk | Where-Object {$_.id -eq $data.storageProfile.osDisk.managedDisk.id} | Select-Object -Unique).Properties.diskSizeGB
+            }
+            else
+            {
+               $OSDisk = if($data.storageProfile.osDisk.vhd.uri){ 'Custom VHD' } else { 'None' }
+               $OSDiskSize = $data.storageProfile.osDisk.diskSizeGB
+            }
+            
             $vmMetrics = $virtualMachineMetrics | Where-Object { $_.Id -eq $vm.id }
             $cpuUtilisationMetric = $vmMetrics | Where-Object { $_.Metric -eq 'Percentage CPU' }
             $memoryAvilableMetric = $vmMetrics | Where-Object { $_.Metric -eq 'Available Memory Bytes' }
@@ -59,6 +71,8 @@ If ($Task -eq 'Processing')
                 'OS'                            = $data.storageProfile.osDisk.osType;
                 'OSName'                        = $data.extended.instanceView.osname;
                 'OSVersion'                     = $data.extended.instanceView.osversion;
+                'OSDisk'                        = $OSDisk;
+                'OSDiskSizeGB'                  = $OSDiskSize;
                 'PowerState'                    = $data.extended.instanceView.powerState.displayStatus;
                 'CpuUtilizationPercent'         = if ($null -ne $cpuUtilisationMetric.MetricPercentile) { $cpuUtilisationMetric.MetricPercentile } else { '0' }
                 'MemoryUtilizationPercent'      = if ($null -ne $memoryAvilableMetric.MetricPercentile) { $memoryTotalGb - ($memoryAvilableMetric.MetricPercentile / (1024 * 1024 * 1024)) } else { '0' }
@@ -91,6 +105,8 @@ else
         $Exc.Add('OSVersion')
         $Exc.Add('ImageReference')
         $Exc.Add('ImageVersion')
+        $Exc.Add('OSDisk')
+        $Exc.Add('OSDiskSizeGB')
         $Exc.Add('HybridBenefit')
         $Exc.Add('PowerState')
         $Exc.Add('AvailabilitySet')
